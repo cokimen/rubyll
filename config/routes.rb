a8d093cbe901388711ea59c9a2ffcb43be240897
compiler_slave <erhancokimen@Erhans-MacBook-Pro.local>
2022-04-28 11:59:27 +0700

    belajar ruby bro

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "about", to: "about#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :books
+        resources :subjects
+      end
+    end
+  end
+end
